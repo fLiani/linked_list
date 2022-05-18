75de3e6074db1c5cb8164ce15cb205c44f97d575
Fabrizio Liani <98763483+fLiani@users.noreply.github.com>
2022-05-18 23:35:37 +0100

    linked list functionality working

--- linked_list.c
+++ linked_list.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-struct sPerson
+typedef struct sPerson
 {
     int age;
     struct sPerson* next;
-};
+} Person;
 
-struct sPerson* getNewPerson(const int age)
+Person* getNewPerson(const int age)
 {
-    struct sPerson* newPerson = NULL;
-    newPerson = malloc(sizeof(struct sPerson));
+    Person* newPerson = NULL;
+    newPerson = malloc(sizeof(Person));
     if(newPerson != NULL)
     {
         newPerson->next = NULL;
@@ -24,7 +25,7 @@ struct sPerson* getNewPerson(const int age)
     return newPerson;
 }
 
-void printPerson(const struct sPerson *person, const char *comment)
+void printPerson(const Person *person, const char *comment)
 {
     if(person == NULL)
     {
@@ -36,13 +37,97 @@ void printPerson(const struct sPerson *person, const char *comment)
     }
 }
 
+void printList(Person* list)
+{
+    printf("Here is your list: \n");
+
+    Person* pt;
+
+    pt = list;
+
+    if(pt == NULL)
+    {
+        printf("List is empty\n");
+        return;
+    }
+    else
+    {
+        for(int i = 0; pt != NULL; i++)
+        {
+            printPerson(pt, "List");
+            pt = pt->next;
+        }
+    }
+}
+
+void cleanUp(Person* list)
+{
+    Person* nextElem;
+    while(list)
+    {
+        nextElem = list->next;
+        printf("Cleaning...\n");
+        free(list);
+        list = nextElem;
+    }
+}
+
 int main()
 {
     printf("** START LINK LIST **\n");
 
-    struct sPerson *head = NULL;
-    struct sPerson *added = NULL;
+    Person *head = NULL;
+    Person *added = NULL;
 
+    char command[64];
+    int age;
+
+    printf("Type quit to exit.\n");
+
+    while(1)
+    {
+        printf("Enter a command or value: ");
+        fgets(command, 64, stdin);
+
+        if(strcmp("quit\n", command) == 0)  // if type quit exit program
+        {
+            printf("Exiting program...\n");
+            return 0;
+        }
+        else if(strcmp("print\n", command) == 0)
+        {
+            printf("Printing...\n");
+            printList(head);
+        }
+        else if(sscanf(command, "%d", &age) != 0)
+        {
+            if(head == NULL)
+            {
+                head = getNewPerson(age);
+
+                if(head != NULL)
+                {
+                    added = head;
+                }
+            }
+            else
+            {
+                added->next = getNewPerson(age);
+
+                if(added->next != NULL)
+                {
+                    added = added->next;
+                }
+            }
+
+            printf("Person age added!\n");
+        }
+    }
+
+    cleanUp(head);
+
+    head = NULL;
+    added = NULL;
 
     printf("** END LINK LIST **\n");
 
